b5274cb60ffb9f679f755c0d1a7e5a8a68104a2b
dxf <1151685410@qq.com>
2025-03-04 10:07:14 +0800

    test

---
+++ src/app/knexfile.ts
@@ -0,0 +1,9 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  client: 'pg',
+  connection: 'postgres://postgres:123456@localhost:5432/erp'
+}
