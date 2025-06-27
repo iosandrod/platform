bbef22464f539515f224ee3fa570e2a6f88c93a6
dxf <1151685410@qq.com>
2025-06-27 15:48:16 +0800

    test

--- src/apps/gantt/index.ts
+++ src/apps/gantt/index.ts
@@ -7,14 +7,12 @@ import Milestone from './milestone.entity'
 import Calendar from './calendar.entity'
 import { defaultEntity } from '@/entity'
 export const ganttConfig = async (app: myFeathers) => {
-  let connection = app.getLocalDefaultConnection('gantt', 1) //
-  const url = new URL(connection)
-  // let password=url.password
+  let connection = app.getLocalDefaultConnection('gantt', 1)
   let d: DataSourceOptions = {
     type: 'postgres',
-    // driver: 'pg', 
     url: connection,
+    synchronize: true, //
     entities: [Task, Resource, TaskAssignment, Milestone, Calendar, ...defaultEntity] //
-  } //
-  return d //
+  }
+  return d
 }
